39548f28dfe48568318705071d794e5c42c45190
БахытжановА <@207-3>
2024-02-23 21:41:59 +0600

    Добавьте файлы проекта.

---
+++ CinemaAPI/Controllers/CinemaController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using CinemaWPF.Models;
+using Dapper;
+
+namespace CinemaAPI.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly string conStr = "Data Source=207-3; Initial Catalog=CinemaDb;Integrated Security=True;TrustServerCertificate=Yes";
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("/Login")]
+        public async Task<bool> Login(string login, string password)
+        {
+            using (SqlConnection db = new SqlConnection(conStr))
+            {
+                DynamicParameters p = new DynamicParameters();
+                p.Add("login", login); p.Add("pwd", password);
+                var res = db.Query("pUser", p, commandType: System.Data.CommandType.StoredProcedure);
+                if (res != null)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        [HttpPost("/GetRole")]
+        public async Task<string> GetRole(string id)
+        {
+            using (SqlConnection db = new SqlConnection(conStr))
+            {
+                return db.ExecuteScalar("pUser;2", id, commandType: System.Data.CommandType.StoredProcedure).ToString();
+            }
+        }
+        [HttpPost("/Register")]
+        public async Task<bool> Register(string login, string password)
+        {
+            using (SqlConnection db = new SqlConnection(conStr))
+            {
+                DynamicParameters p = new DynamicParameters();
+                p.Add("login", login); p.Add("pwd", password);
+                var res = db.Query("pUser;3", p, commandType: System.Data.CommandType.StoredProcedure);
+                if (res != null)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        /*
+         create table Users(
+id int identity,
+username nvarchar(1000),
+pwd nvarchar(1000),
+accessRole nvarchar(1000)
+)
+
+create table movie(
+id int identity,
+actors nvarchar(max),
+genres nvarchar(1000),
+seanceId int,
+title nvarchar(1000),
+about nvarchar(1000),
+writer nvarchar(1000),
+movieTime nvarchar(1000)
+)
+drop table seance
+
+create table seance(
+id int identity,
+price int,
+seatings int,
+seanceTime nvarchar(1000)
+)
+
+create table places(
+id int identity,
+seanceId int,
+isTaken bit
+)
+
+create table ticket(	
+id int identity,
+seanceId int,
+movieId int,
+movieName nvarchar(1000),
+price int 
+)
+
+create proc pUser
+@login nvarchar(1000),
+@pwd nvarchar(1000)
+as
+select * from Users where username like @login and pwd like @pwd
+
+insert into Users values('john','john','user')
+
+create proc pUser;2
+@id int  
+as
+select accessRole from Users where id = @id
+
+create proc pUser;3 
+@login nvarchar(1000),
+@pwd nvarchar(1000),
+@role nvarchar(1000)
+as
+insert into Users
+         */
+
+    }
+}
---
+++ CinemaAPI/Models/Movie.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaWPF.Models
+{
+	public class Users
+	{
+		public int id { get; set; }
+		public string username { get; set; }
+		public string pwd { get; set; }
+		public string accessRole { get; set; }
+
+		public Users(int id_, string username_, string pwd_, string accessRole_)
+		{
+			this.id = id_;
+			this.username = username_;
+			this.pwd = pwd_;
+			this.accessRole = accessRole_;
+		}
+	}
+	public class movie
+	{
+		public int id { get; set; }
+		public string actors { get; set; }
+		public string genres { get; set; }
+		public int seanceId { get; set; }
+		public string title { get; set; }
+		public string about { get; set; }
+		public string writer { get; set; }
+		public string movieTime { get; set; }
+
+		public movie(int id_, string actors_, string genres_, int seanceId_, string title_, string about_, string writer_, string movieTime_)
+		{
+			this.id = id_;
+			this.actors = actors_;
+			this.genres = genres_;
+			this.seanceId = seanceId_;
+			this.title = title_;
+			this.about = about_;
+			this.writer = writer_;
+			this.movieTime = movieTime_;
+		}
+	}
+	public class seance
+	{
+		public int id { get; set; }
+		public int price { get; set; }
+		public int seatings { get; set; }
+		public string seanceTime { get; set; }
+
+		public seance(int id_, int price_, int seatings_, string seanceTime_)
+		{
+			this.id = id_;
+			this.price = price_;
+			this.seatings = seatings_;
+			this.seanceTime = seanceTime_;
+		}
+	}
+	public class places
+	{
+		public int id { get; set; }
+		public int seanceId { get; set; }
+		public bool isTaken { get; set; }
+
+		public places(int id_, int seanceId_, bool isTaken_)
+		{
+			this.id = id_;
+			this.seanceId = seanceId_;
+			this.isTaken = isTaken_;
+		}
+	}
+	public class ticket
+	{
+		public int id { get; set; }
+		public int seanceId { get; set; }
+		public int movieId { get; set; }
+		public string movieName { get; set; }
+		public int price { get; set; }
+
+		public ticket(int id_, int seanceId_, int movieId_, string movieName_, int price_)
+		{
+			this.id = id_;
+			this.seanceId = seanceId_;
+			this.movieId = movieId_;
+			this.movieName = movieName_;
+			this.price = price_;
+		}
+	}
+}
